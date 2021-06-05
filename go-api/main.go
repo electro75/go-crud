30f0a35ce637f17dfff0631e5753e0e855dc4788
Sahil Naikdhure <sanaikdhure@gmail.com>
2021-06-05 12:55:49 +0530

    fix issue and add base struct

--- go-api/main.go
+++ go-api/main.go
@@ -6,16 +6,29 @@ import (
 	"net/http"
 )
 
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json: "desc"`
+	Content string `json: "content"`
+}
+
+var Articles []Article
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to page")
 	fmt.Println("Endpoint hit: homepage")
 }
 
 func handleRequests() {
-	http.handleFunc("/", homePage)
+	http.HandleFunc("/", homePage)
 	log.Fatal(http.ListenAndServe(":1000", nil))
 }
 
 func main() {
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+
 	handleRequests()
 }
